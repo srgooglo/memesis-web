3a83f8220396088bf0a55373919e0ba5979cb355
srgooglo <srgooglo@ragestudio.net>
2021-12-07 19:20:26 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import * as antd from "antd"
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      
+    </div>
+  )
+}
+
+export default App
